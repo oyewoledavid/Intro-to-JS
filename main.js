b1a899250e59b5ee42cf2d993604561b5ef44917
oyewoledavid <64299405+oyewoledavid@users.noreply.github.com>
2022-06-22 09:52:33 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let firstName = "Oyewole ";
+let lastName = "David ";
+let otherName = "Oluwafemi ";
+let height = "6feet";
+let country = "Nigeria";
+
+alert(
+  "Name: " +
+    firstName +
+    lastName +
+    otherName +
+    "\n" +
+    "Height: " +
+    height +
+    "\n" +
+    "Country: " +
+    country
+);
